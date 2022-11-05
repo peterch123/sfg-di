ba91a9f7a7013b7f4949d3718675a50a1913182a
peterc <peter.chivers@gmail.com>
2022-11-05 13:05:45 +0000

    DI Tests

---
+++ src/test/java/guru/springframework/sfgdi/controllers/ConstructedInjectedControllerTest.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConstructedInjectedControllerTest {
+
+    ConstructedInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new ConstructedInjectedController(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
