5cf4e47b237d22d2643c67a1e8e3b4b3c6d38ae1
peterc <peter.chivers@gmail.com>
2022-11-05 13:23:34 +0000

    Add Spring DI Config to controller classes and service

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.ConstructedInjectedController;
 import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +19,19 @@ public class SfgDiApplication {
 		String greeting = myController.sayHello();
 
 		System.out.println(greeting);
+
+		System.out.println("propertyInjectedController test");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println("propertyInjectedController " + propertyInjectedController.getGreeting());
+
+		System.out.println("setterInjectedController test");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println("setterInjectedController " + setterInjectedController.getGreeting());
+
+		System.out.println("constructedInjectedController test");
+		ConstructedInjectedController constructedInjectedController = (ConstructedInjectedController) ctx.getBean("constructedInjectedController");
+		System.out.println("constructedInjectedController " + constructedInjectedController.getGreeting());
+
 	}
 
 }
